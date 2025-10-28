39f93826a7675fdaf28ec4c5353ed9174a183a7f
allendimal123 <dimalclydeallen@gmail.com>
2025-10-28 10:17:15 +0800

    feat:login

--- login.php
+++ login.php
@@ -6,12 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
-        <label>Username</label>
-        <input type="text" id="username" required></input>
-        <label>Password</label>
-        <input type="password" id="password" required minlength="6">
-        <input type="button" value="Submit">
+    <form id="login">
+        <input type="text" id="username" placeholder="Username"></input> <br>
+        <input type="password" id="password" minlength="6" placeholder="Password"><br>
+        <a href="">Forgot Your Password?</a><br>
+        <input type="submit" value="Submit">
     </form>
 </body>
+
+<script>
+    document.getElementById("login").onsubmit = function(e){
+        if(
+            document.getElementById("username").value == "" || 
+            document.getElementById("password").value == "")
+            {
+            alert("Please fill out the neccessary fields.");
+            e.preventDefault();
+        }
+    };
+</script>
 </html>
