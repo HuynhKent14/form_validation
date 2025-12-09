d1ab971e397e02c0709abd9ecdfea55302df1c68
Rainer David <rainermanlapazdavid@gmail.com>
2025-12-09 23:25:21 +0800

    feat: details page

---
+++ details.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Details</title>
+    <link rel="stylesheet" href="Details.css">
+    </head>
+<body>
+
+<div class="header">
+  <div class="topCon">
+      <div class="upperCon">
+      <div class="siteTitle">Le Critique </div>
+      <img class="dashIcon" src="dashIcon.png" alt="">
+  </div>
+            
+            
+<div class="movieDetails">
+  <div class="movieImage"> 
+    <img src="movieImage.jpg" alt="">
+  </div>
+
+    <div class="movieInfo">
+      <img class="ratings" src="starImage.png" alt="">
+      <div class="movieTitle">Kimetsu No Yaiba</div>
+
+        <div class="movieDirector">
+          <span class="directorLabel">Director:</span> 
+          <span class="movieDirector">Haruo Sotozaki</span>
+        </div>
+
+          <div class="yearReleased">2023</div> 
+                
+            <div class="synopsisSec">
+              <h2>Synopsis</h2>
+              <p class="synopsisPar">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+          </div>
+        </div>
+        
+        <hr class="separator">
+
+        <div class="commentSec">
+          <div class="commentBox">
+            <input type="text" placeholder="Write a comment">
+          </div>
+            <img class="commentIcon" src="commentIcon.png" alt="">
+        </div>  
+
+        <div class="userComment">
+          <img class="userPfp" src="userPfp.jpg" alt="">
+            <div class="commentDetails">
+              <div class="userName">SenpaiDaisuki_20</div>
+              <div class="commentDate">12/8/2025</div>
+                <p class="commentText">
+                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+</body>
+</html>
