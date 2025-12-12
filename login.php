20c41424a58c282a2874f13b1b9e6f6e3b9ea514
HuynhKent14 <huynhdeleonkent@gmail.com>
2025-12-12 22:23:25 +0800

    feat: visitor fix: css links

--- login.php
+++ login.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
-    <title>Document</title>
+    <title>Log In</title>
     <link rel="stylesheet" href="css/login.css">
 </head>
 
@@ -25,7 +24,7 @@
             </div>
             <br>
             <span></span>
-            <a href="">Forgot Your Password?</a>
+            <a class="visitor">Proceed As Visitor?</a>
             <input type="submit" value="Submit"> <br>
             <a onclick=toggleForms(1)>Doesn't have an account?</a>
         </div>
